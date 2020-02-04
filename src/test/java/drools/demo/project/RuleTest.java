ac61d28335edaf815ff5d91892f3afeb7a37e067
Rafael T. C. Soares <rafaelcba@gmail.com>
2020-02-04 09:34:42 -0500

    first commit

---
+++ src/test/java/drools/demo/project/RuleTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package drools.demo.project;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import drools.demo.project.Item.Category;
+
+public class RuleTest {
+    static final Logger LOG = LoggerFactory.getLogger(RuleTest.class);
+
+    @Test
+    public void test() {
+        KieServices kieServices = KieServices.Factory.get();
+
+        KieContainer kContainer = kieServices.getKieClasspathContainer();
+        Results verifyResults = kContainer.verify();
+        for (Message m : verifyResults.getMessages()) {
+            LOG.info("{}", m);
+        }
+
+        LOG.info("Creating kieBase");
+        KieBase kieBase = kContainer.getKieBase();
+
+        LOG.info("There should be rules: ");
+        for ( KiePackage kp : kieBase.getKiePackages() ) {
+            for (Rule rule : kp.getRules()) {
+                LOG.info("kp " + kp + " rule " + rule.getName());
+            }
+        }
+
+        LOG.info("Creating kieSession");
+        KieSession session = kieBase.newKieSession();
+        LOG.info("Feeding kieSession");
+        Item item1 = new Item("A", 123.0, 150.0);
+        LOG.info("Item category {}", item1.getCategory().toString());
+        session.insert(item1);
+
+        LOG.info("Now running data");
+        int firedRules = session.fireAllRules(10);
+        LOG.info("Number of fired rules {}", firedRules);
+        LOG.info("Item Category after rules {}", item1.getCategory().toString());
+
+        assertEquals("Low range category", Category.LOW_RANGE, item1.getCategory());
+
+    }
+}
